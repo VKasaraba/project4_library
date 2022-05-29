5c637cad66cda0cd993e505cad8e4d005920109d
Taras <tarasfqx@gmail.com>
2022-05-29 22:43:36 +0300

    created users app and added custom user model

---
+++ library/apps/users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from datetime import date
+from library.apps.users.managers import UserManager
+
+
+class StatusChoices(models.TextChoices):
+    LIBRARIAN = 'librarian'
+    REGULAR_CUSTOMER = 'regular customer'
+    CULTURAL_FIGURE = 'cultural figure'
+
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=180, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=30, null=True)
+    status = models.CharField(max_length=30, choices=StatusChoices.choices)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    # def save(self, *args, **kwargs):
+    #     raw_password = self.password
+    #     self.set_password(raw_password)
+    #     return super().save(*args, **kwargs)
