01ed14d176ec8891f139c6f96f57b5122db67d7d
Taras <tarasfqx@gmail.com>
2022-05-31 20:54:58 +0300

    added serializer and view for user

--- library/apps/users/views.py
+++ library/apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import (
+    RetrieveUpdateDestroyAPIView, ListCreateAPIView,
+)
+from library.apps.users.models import User
+from library.apps.users.serializers import UserSerializer
+from library.apps.utils.custom_exception_handler import handle_library_exceptions
+from rest_framework.permissions import IsAuthenticated
+
+
+@handle_library_exceptions
+class UserListCreateAPIView(ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+@handle_library_exceptions
+class UserRetrieveUpdateAPIView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
