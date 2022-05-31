2fbd84b792699d2de995dd17daf94dea66e207fd
Andrii <777andriypeleno777@gmail.com>
2022-05-31 20:59:32 +0300

    added exceotion handler decorator and added permission classe

--- library/apps/books/views/author_view.py
+++ library/apps/books/views/author_view.py
@@ -3,13 +3,19 @@ from rest_framework.generics import (
 )
 from library.apps.books.models.author import Author
 from library.apps.books.serializers.author_serializer import AuthorSerializer
+from library.apps.utils.custom_exception_handler import handle_library_exceptions
+from rest_framework.permissions import IsAuthenticated
 
 
+@handle_library_exceptions
 class AuthorListCreateAPIView(ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
 
 
+@handle_library_exceptions
 class AuthorRetrieveUpdateAPIView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
--- library/apps/books/views/publisher_view.py
+++ library/apps/books/views/publisher_view.py
@@ -3,13 +3,19 @@ from rest_framework.generics import (
 )
 from library.apps.books.models.publisher import Publisher
 from library.apps.books.serializers.publisher_serializer import PublisherSerializer
+from library.apps.utils.custom_exception_handler import handle_library_exceptions
+from rest_framework.permissions import IsAuthenticated
 
 
+@handle_library_exceptions
 class PublisherListCreateAPIView(ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
     serializer_class = PublisherSerializer
     queryset = Publisher.objects.all()
 
 
+@handle_library_exceptions
 class PublisherRetrieveUpdateAPIView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     serializer_class = PublisherSerializer
     queryset = Publisher.objects.all()
