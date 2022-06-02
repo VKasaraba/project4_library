52648bcc96d3b659506ee8b35fe656a54c770159
Andrii <777andriypeleno777@gmail.com>
2022-06-02 16:35:07 +0300

    added new configs

--- library/settings.py
+++ library/settings.py
@@ -143,3 +143,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+}
