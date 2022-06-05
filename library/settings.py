0eae9ce8af52bf1a63262852ca023fa7a21a10f6
Maksym-Fedir Zvarych <2811maximus@gmail.com>
2022-06-05 16:37:21 +0300

    Changed lifetime for jwt access token

--- library/settings.py
+++ library/settings.py
@@ -150,7 +150,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
