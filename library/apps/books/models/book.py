5c98ff4bac75d3f59528cb22f1df45ad96b4f008
Andrii <tarasfqx@gmail.com>
2022-06-06 20:33:40 +0300

    added collateral_price to book model and order serializer

--- library/apps/books/models/book.py
+++ library/apps/books/models/book.py
@@ -1,6 +1,8 @@
 from statistics import mode
 from django.db import models
 
+from library.apps.utils.custom_exception import raise_library_exception
+
 
 class ConditionChoise(models.TextChoices):
     NO_DAMAGE = 'No damage'
@@ -19,9 +21,10 @@ class Book(models.Model):
     condition = models.CharField(max_length=50, choices=ConditionChoise.choices, default=ConditionChoise.NO_DAMAGE)
     comment = models.TextField(null=True)
     original_weekly_rent_price = models.FloatField()
+    original_collateral_price = models.FloatField()
 
     @staticmethod
-    def _calculate_price_price(price, condition):
+    def _calculate_price(price, condition):
         if condition == ConditionChoise.MINOR_DAMAGE:
             return price * 0.8
         elif condition == ConditionChoise.SIGNIFICANT_DAMAGE:
@@ -29,4 +32,15 @@ class Book(models.Model):
         return price
 
     def get_weekly_rent_price(self):
-        return Book._calculate_price_price(self.original_weekly_rent_price, self.condition)
+        return Book._calculate_price(self.original_weekly_rent_price, self.condition)
+
+    def get_collateral_price(self):
+        return Book._calculate_price(self.original_collateral_price, self.condition)
+
+    def reduce_number_of_copies(self, num=1):
+        if num > self.number_of_copies:
+            raise_library_exception(400, 'Not enough copies abailable')
+        self.number_of_copies -= num
+
+    def increase_number_of_copies(self, num=1):
+        self.number_of_copies += num
